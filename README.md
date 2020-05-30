c1def478e006d20df55fb596610b95e2fbfc8a1f
Tomás Salazar Leal <atomsmail@gmail.com>
2020-05-30 01:06:17 -0500

    Add first version hans on fast api

---
+++ README.md
@@ -0,0 +1,16 @@
+## [Fast API]([https://fastapi.tiangolo.com/) hans on
+
+### Develop
+#### Setup
+```
+pipenv install
+```
+### Run
+```
+pipenv shell
+uvicorn main:app --reload
+```
+
+### API Doc
+*  http://127.0.0.1:8000/docs
+*  http://127.0.0.1:8000/redocs
---
+++ sql/models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    items = relationship("Item", back_populates="owner", lazy=True)
+
+    def __str__(self):
+        # Override to print a readable string presentation of your object
+        # below is a dynamic way of doing this without explicity constructing the string manually
+        return ', '.join(['{key}={value}'.format(key=key, value=self.__dict__.get(key)) for key in self.__dict__])
+
+
+
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("User", back_populates="items")
